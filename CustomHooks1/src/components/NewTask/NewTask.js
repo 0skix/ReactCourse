98c101a94848251aa957ae66add6dc76bed563ed
Oskar Zera <46406478+0skix@users.noreply.github.com>
2023-02-15 14:48:43 +0100

    Update NewTask.js

--- CustomHooks1/src/components/NewTask/NewTask.js
+++ CustomHooks1/src/components/NewTask/NewTask.js
@@ -12,7 +12,7 @@ const NewTask = (props) => {
 		setError(null);
 		try {
 			const response = await fetch(
-				"https://react-http-c9ed6-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
+				"",
 				{
 					method: "POST",
 					body: JSON.stringify({ text: taskText }),
