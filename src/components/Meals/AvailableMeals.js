cac440fbf839d9fdfc9c19982250ebae18fa26a9
0skix <oskyzera@gmail.com>
2023-02-16 15:43:28 +0100

    solving issue with Portals

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,38 +3,18 @@ import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 import { useEffect, useState } from "react";
 
-const DUMMY_MEALS = [
-	{
-		id: "m1",
-		name: "Sushi",
-		description: "Finest fish and veggies",
-		price: 22.99,
-	},
-	{
-		id: "m2",
-		name: "Schnitzel",
-		description: "A german specialty!",
-		price: 16.5,
-	},
-	{
-		id: "m3",
-		name: "Barbecue Burger",
-		description: "American, raw, meaty",
-		price: 12.99,
-	},
-	{
-		id: "m4",
-		name: "Green Bowl",
-		description: "Healthy...and green...",
-		price: 18.99,
-	},
-];
-
 const AvailableMeals = () => {
 	const [meals, setMeals] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [httpError, setHttpError] = useState(null);
 	useEffect(() => {
 		const fetchMeals = async () => {
 			const response = await fetch(process.env.REACT_APP_FIREBASE_URL);
+
+			if (!response.ok) {
+				throw new Error("Something went wrong!");
+			}
+
 			const responseData = await response.json();
 
 			const loadedMeals = [];
@@ -48,9 +28,12 @@ const AvailableMeals = () => {
 				});
 			}
 			setMeals(loadedMeals);
+			setLoading(false);
 		};
 
-		fetchMeals();
+		fetchMeals().catch((error) => {
+			setHttpError(error.message);
+		});
 	}, []);
 
 	const mealsList = meals.map((meal) => (
@@ -66,7 +49,13 @@ const AvailableMeals = () => {
 	return (
 		<section className={classes.meals}>
 			<Card>
-				<ul>{mealsList}</ul>
+				{httpError ? (
+					<p className={classes.MealsLoadingError}>Failed to fetch</p>
+				) : loading ? (
+					<p className={classes.MealsLoading}>Loading...</p>
+				) : (
+					<ul>{mealsList}</ul>
+				)}
 			</Card>
 		</section>
 	);
